038cc61d15f6eef9ee0b31df6a03094deeb37a76
wuqingshan <wuqingshan@xylink.com>
2022-04-28 23:07:23 +0800

    chore: 🛠  update configs

--- docs/.vuepress/config/head.config.ts
+++ docs/.vuepress/config/head.config.ts
@@ -3,25 +3,25 @@
  ******************************************************************************************/
 
 export default [
-  ["link", { rel: "icon", href: `/img/favicon.ico` }],
-  ["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
-  ["meta", { name: "keywords", content: "机器学习,深度学习" }],
-  // ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站长验证（你可以去掉）
-  ["meta", { name: "theme-color", content: "#06403F" }], // 移动浏览器主题颜色
-  [
-    "link",
-    {
-      rel: "stylesheet",
-      href: "//at.alicdn.com/t/font_3314748_9xij1pv3h4i.css",
-    },
-  ],
+	["link", { rel: "icon", href: `/img/favicon.ico` }],
+	["meta", { name: "referrer", content: "no-referrer-when-downgrade" }],
+	["meta", { name: "keywords", content: "机器学习,深度学习" }],
+	// ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站长验证（你可以去掉）
+	["meta", { name: "theme-color", content: "#06403F" }], // 移动浏览器主题颜色
+	[
+		"link",
+		{
+			rel: "stylesheet",
+			href: "//at.alicdn.com/t/font_3314748_9xij1pv3h4i.css",
+		},
+	],
 
-  // [
-  //   'script',
-  //   {
-  //     'data-ad-client': 'ca-pub-7828333725993554',
-  //     async: 'async',
-  //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-  //   },
-  // ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
+	// [
+	//   'script',
+	//   {
+	//     'data-ad-client': 'ca-pub-7828333725993554',
+	//     async: 'async',
+	//     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+	//   },
+	// ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
 ];

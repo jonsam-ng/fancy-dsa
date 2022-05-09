425da7954f5a0d22af0e30aff45388c0b794d358
wuqingshan <wuqingshan@xylink.com>
2022-05-09 12:13:44 +0800

    perf: ⚡enhance ui and seo

--- docs/.vuepress/config/head.config.ts
+++ docs/.vuepress/config/head.config.ts
@@ -15,6 +15,32 @@ export default [
 			href: "//at.alicdn.com/t/font_3314748_9xij1pv3h4i.css",
 		},
 	],
+	// PWA
+	["link", { rel: "icon", href: "/img/favicon.ico", type: "image/x-icon" }],
+	["link", { rel: "manifest", href: "/img/manifest.json" }],
+	["meta", { name: "theme-color", content: "#06403F" }], // 移动浏览器主题颜色
+	["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+	["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+	[
+		"link",
+		{ rel: "apple-touch-icon", href: "/img/android-chrome-192x192.png" },
+	],
+	[
+		"link",
+		{
+			rel: "mask-icon",
+			href: "/icons/safari-pinned-tab.svg",
+			color: "#3eaf7c",
+		},
+	],
+	[
+		"meta",
+		{
+			name: "msapplication-TileImage",
+			content: "/icons/msapplication-icon-144x144.png",
+		},
+	],
+	["meta", { name: "msapplication-TileColor", content: "#000000" }],
 
 	// [
 	//   'script',
